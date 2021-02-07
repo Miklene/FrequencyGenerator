94a91cedad8e0424118019ebff3521d15fb4c9a9
Kotov Mikhail <kotov-mikhail@bk.ru>
2021-02-07 13:01:46 +0300

    CreateFactory

---
+++ app/src/main/java/com/miklene/frequencygenerator/wave/SimpleWaveFactory.java
@@ -0,0 +1,14 @@
+package com.miklene.frequencygenerator.wave;
+
+public class SimpleWaveFactory {
+
+    public Wave createWave(WaveType waveType, float frequency, double amplitude){
+        Wave wave = null;
+        if(waveType.equals(WaveType.SINE))
+            wave = new SineWave(frequency,amplitude);
+        /*if(waveType.equals(WaveType.SAWTOOTH))
+            wave= new SawtoothWave(frequency, amplitude);*/
+        return wave;
+    }
+
+}
